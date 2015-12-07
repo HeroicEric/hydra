ee40ab12a4f3a884175606003b6a238dc480d8b0
Adam Sheehan <a.t.sheehan@gmail.com>
2015-12-06 19:32:22 -0500

    Add library for interfacing with Kafka
    Followed installation instructions in the README for [kafka_ex][0].
    It assumes a kafka server is running locally on port 9092.
    
    [0]: https://github.com/kafkaex/kafka_ex/blob/master/README.md

--- backend/mix.exs
+++ backend/mix.exs
@@ -19,7 +19,7 @@ defmodule Hydra.Mixfile do
   def application do
     [mod: {Hydra, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :kafka_ex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -35,7 +35,8 @@ defmodule Hydra.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:kafka_ex, "~> 0.2.1"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
